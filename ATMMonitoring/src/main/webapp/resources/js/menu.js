7dc2cddd42d2485be5c8afd854df14abbd8204fd
Rafael Luque <rafael.luque@osoco.es>
2013-11-06 10:28:31 +0100

    Fix colorbox used with dynamic terminal photos

--- ATMMonitoring/src/main/webapp/resources/js/menu.js
+++ ATMMonitoring/src/main/webapp/resources/js/menu.js
@@ -39,7 +39,7 @@ $(function() {
 // Colorbox
 	$(".iframe").colorbox({iframe:true, width:"90%", height:"90%"}); // Iframes
 	$(".iframe_s").colorbox({iframe:true, width:"640", height:"480px", maxWidth:"100%", maxHeight:"100%"}); // Iframes
-	$(".colorbox").colorbox({rel:'group1', maxWidth:"100%", maxHeight:"100%"}); // Photo
+	$(".colorbox").colorbox({rel:'group1', maxWidth:"100%", maxHeight:"100%", photo: true}); // Photo
 	$(".inline").colorbox({inline:true,  width:"640", height:"480px"}); // pop-ups en la misma pagina
 
 // Instalacion ATM checkbox
