538080379e8f952b07ca8e2dc513eaeffb8df76e
Jorge Lopez Fernandez <lopez.fernandez.jorge@gmail.com>
2014-03-15 21:42:53 +0100

    Added javadoc to TrialEndedException

--- ATMMonitoring/src/main/java/com/ncr/ATMMonitoring/utils/TrialEndedException.java
+++ ATMMonitoring/src/main/java/com/ncr/ATMMonitoring/utils/TrialEndedException.java
@@ -1,15 +1,39 @@
 package com.ncr.ATMMonitoring.utils;
 
+/**
+ * The TrialEnded exception.
+ * 
+ * Exception raised (and always internally caught and managed) when the software
+ * trial period has reached its end.
+ * 
+ * @author Jorge López Fernández (lopez.fernandez.jorge@gmail.com)
+ */
 public class TrialEndedException extends Exception {
 
+    /** The serial field */
+    private static final long serialVersionUID = 8973608074217634386L;
+
+    /**
+     * Constructor that simply accepts a message.
+     * 
+     * @param message
+     *            the exception message
+     */
     public TrialEndedException(String msg) {
 	super(msg);
     }
 
+    /**
+     * Constructor that accepts both another Throwable (usually an Exception) as
+     * the cause of the constructed one and a message.
+     * 
+     * @param message
+     *            the exception message
+     * @param exc
+     *            the cause of this exception
+     */
     public TrialEndedException(String msg, Exception e) {
 	super(msg, e);
     }
-
-    private static final long serialVersionUID = 8973608074217634386L;
 
 }
