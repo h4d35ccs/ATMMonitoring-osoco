b4a727401a8dc8df02f6c965008b83621ff521fd
otto abreu <ottoabreu@gmail.com>
2014-06-18 13:11:42 +0200

    Fixed bug related to map the network when a leaf is down. now each node respond, is not wating to reach the leaf

--- ATMMonitoring/src/main/java/com/ncr/ATMMonitoring/serverchain/message/specific/strategy/imp/NetworkMappingRequestStrategy.java
+++ ATMMonitoring/src/main/java/com/ncr/ATMMonitoring/serverchain/message/specific/strategy/imp/NetworkMappingRequestStrategy.java
@@ -6,6 +6,7 @@ import org.apache.log4j.Logger;
 
 import com.ncr.ATMMonitoring.routertable.RouterTableHandler;
 import com.ncr.ATMMonitoring.serverchain.message.specific.incoming.NetworkMappingResponse;
+import com.ncr.serverchain.message.specific.SpecificMessage;
 import com.ncr.serverchain.message.specific.strategy.BroadcastType;
 import com.ncr.serverchain.message.specific.strategy.imp.BaseStrategy;
 import com.ncr.serverchain.message.wrapper.IncomingMessage;
@@ -15,9 +16,15 @@ import com.ncr.serverchain.message.wrapper.MessageWrapper;
  * <pre>
  * Strategy that process NetworkMappingRequest messages.
  * 
- * This strategy travels all the nodes until it reaches the leaf, there it creates 
+ * This strategy travels all the nodes and generate 
  * a response message ( NetworkMappingResponse)  and puts the information of the leaf node inside.
- * After that the message is returned to the parent ( BroadcastType.TURN_BACK)
+ * After that the message is returned to the parent
+ * 
+ * In the middle nodes the Request message will be broadcasted to all the children and also will generate a  response
+ * to send to the parent node (BroadcastType.TWO_WAY) 
+ * will be generated in the node.
+ * 
+ * In the leaf will generate the response using (BroadcastType.TURN_BACK)
  * 
  * @author Otto Abreu
  * <pre>
@@ -48,16 +55,15 @@ public class NetworkMappingRequestStrategy extends BaseStrategy {
     @Override
     public void processSpecificMessage() {
 
-	if (this.isLeaf()) {
-	    this.generateResponseInLeaf();
+	if (this.isLeaf() || this.isMiddle()) {
+	    this.generateResponse();
 	    logger.debug("NetworkMap Response generated");
 	}
 
     }
 
-  
 
-    private void generateResponseInLeaf() {
+    private void generateResponse() {
 	 
 	this.response = new NetworkMappingResponse();
 	 String nodeUrlAndPort =  this.nodeInformation.getLocalUrl();
@@ -80,17 +86,38 @@ public class NetworkMappingRequestStrategy extends BaseStrategy {
 	if(this.isLeaf()){
 	    
 	    broadcast = BroadcastType.TURN_BACK;
+	    
+	}else if(this.isMiddle()){
+	   
+	    broadcast = BroadcastType.TWO_WAY;
 	}
 	
 	return broadcast;
     }
-    
+    /*
+     * (non-Javadoc)
+     * @see com.ncr.serverchain.message.specific.strategy.imp.BaseStrategy#getTurnBackMessage()
+     */
     @Override
     public MessageWrapper getTurnBackMessage() {
-	
+
+	return this.getResponseMessageWrapper();
+    }
+    
+    private MessageWrapper getResponseMessageWrapper(){
 	MessageWrapper turnbackMessage =  new IncomingMessage("Network mapping response",new Date().getTime());
 	turnbackMessage.setSpecificMessage(this.response);
 	return turnbackMessage;
     }
 
+    /*
+     * (non-Javadoc)
+     * @see com.ncr.serverchain.message.specific.strategy.imp.BaseStrategy#getChangeDirectionMessageInTwoWay()
+     */
+    @Override
+   public SpecificMessage getChangeDirectionMessageInTwoWay(){
+    
+       return this.response;
+       
+   }
 }
